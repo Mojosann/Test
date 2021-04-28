5fedbf91c0e6b04199241d3c4922ce6cb3a059d3
Mojosann <74448163+Mojosann@users.noreply.github.com>
2021-04-28 14:06:51 +0900

    Add files via upload
    ver.2
    put random date and get the week's date.
    error: misusing function 'list()' before 'zip()', because the value return from zip() is list.

---
+++ test 05/get_week_date_v0422.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+
+def get_week_date(date=datetime.today().strftime('%Y-%m-%d')):
+
+	# string translate to date
+	date_date = datetime.strptime(date,'%Y-%m-%d')
+
+	# get week_delta list (ex: sunday isoweekday() == 0, ...)
+	week_delta_list = []
+	for delta in range(7):
+		week_delta_list.append(date_date.isoweekday() - delta)
+	
+	# get week_date list (date - week's delta)
+	week_date_list = []
+	for date_delta in week_delta_list:
+		week_date_list.append((date_date - timedelta(days = date_delta)).strftime('%Y-%m-%d'))
+
+	# set week_name order
+	week_name_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']	
+	
+	# return resault
+	return list(zip(week_name_list, week_date_list))
+	# return dict(zip(week_date_list, week_name_list))
+
+
+def main():
+
+	query_date = '2021-01-01'
+	week_date = get_week_date(query_date)
+	print('%s week date: %s' % (query_date, week_date))
+
+	query_date = '2021-04-17'
+	week_date = get_week_date(query_date)
+	print('%s week date: %s' % (query_date, week_date))
+
+	query_date = '2021-07-28'
+	week_date = get_week_date(query_date)
+	print('%s week date: %s' % (query_date, week_date))
+
+		
+if __name__ == '__main__':
+	
+	main()
