9083057cb0a30d127fbf253d388ac28eaa452dba
Mojosann <74448163+Mojosann@users.noreply.github.com>
2021-05-01 14:48:31 +0900

    Add files via upload
    ver.adjust_date_format

---
+++ test 05/get_date_function_0429_date_type.py
@@ -0,0 +1,80 @@
+# ver.adjust_date_format
+
+from datetime import datetime, timedelta
+
+def get_week_date(date=datetime.today().strftime('%Y-%m-%d')):
+
+	# string translate to date
+	date_date = datetime.strptime(date,'%Y-%m-%d')
+
+	# get week_delta list (ex: sunday isoweekday() == 0, ...)
+	week_delta_list = []
+	for delta in range(7):
+		week_delta_list.append(date_date.isoweekday() - delta)
+	
+	# get week_date list (date - week's delta)
+	week_date_list = []
+	for date_delta in week_delta_list:
+		new_date = (date_date - timedelta(days = date_delta)).strftime('%Y%m%d') # set new date format 
+		week_date_list.append(new_date)
+		
+	# set week_name order
+	week_name_list = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']	
+	
+	# combine week_name / week_date into dict
+	resault = {}
+	for count in range(7):
+		resault[week_name_list[count]] = week_date_list[count]
+	return resault
+	
+	
+def get_weekday_by_date(date, week_day_name):
+	
+	# get date's week date dict
+	date_dict = get_week_date(date)
+	
+	# get week day name's date
+	if week_day_name in date_dict:
+		week_day_name = date_dict[week_day_name] #str
+		return week_day_name
+
+
+def get_monday_by_date(date):
+		
+	# get Monday's date
+	monday_date = get_weekday_by_date(date, 'Monday')
+	return monday_date.replace('-', '')
+
+
+def main():
+	
+	# example 1
+	query_date = '2021-03-08'
+	query_week_day_name = 'Sunday'
+	weekday = get_weekday_by_date(query_date, query_week_day_name)
+	print("%s's %s is %s" % (query_date, query_week_day_name, weekday))
+	
+	monday = get_monday_by_date(query_date)
+	print("%s's Monday is %s" % (query_date, monday))
+
+	# example 2
+	query_date = '2021-08-15'
+	query_week_day_name = 'Thursday'
+	weekday = get_weekday_by_date(query_date, query_week_day_name)
+	print("%s's %s is %s" % (query_date, query_week_day_name, weekday))
+	
+	monday = get_monday_by_date(query_date)
+	print("%s's Monday is %s" % (query_date, monday))
+
+	# example 3
+	query_date = '2021-04-07'
+	query_week_day_name = 'Friday'
+	weekday = get_weekday_by_date(query_date, query_week_day_name)
+	print("%s's %s is %s" % (query_date, query_week_day_name, weekday))
+	
+	monday = get_monday_by_date(query_date)
+	print("%s's Monday is %s" % (query_date, monday))
+
+
+if __name__ == '__main__':
+	main()
